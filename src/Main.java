607bbcfaaef97952adbfb78f0766a902743f1763
Asghar <asgharkrk@gmail.com>
2023-03-22 19:56:05 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+            Thread.sleep(4000);
+        }
+    }
+}
