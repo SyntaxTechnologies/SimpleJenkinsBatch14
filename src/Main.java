a60706daed8a130138b70881f6e384e2112d18f9
Asghar <asgharkrk@gmail.com>
2023-03-22 21:02:35 -0400

    Changed the code to 15 instead of 10

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println("Hello World");
             Thread.sleep(4000);
+            // Break till 8:50
         }
     }
 }
