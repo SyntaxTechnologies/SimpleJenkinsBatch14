211d22d3dc5e3cc322c62a89793e641b69274154
Asghar <asgharkrk@gmail.com>
2023-03-22 22:03:54 -0400

    Pipeline should be trigger

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 16; i++) {
             System.out.println("Hello World");
             Thread.sleep(4000);
             // Break till 8:50
